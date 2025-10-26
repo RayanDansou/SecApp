40e186464c74b2ec5b2a7271e0249a3430181ce4
Rayan DANSOU <dansourayan@gmail.com>
2025-10-26 08:49:03 +0100

    [Done] Dark mode

--- backend/secapp/settings.py
+++ backend/secapp/settings.py
@@ -141,8 +141,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS Configuration
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://localhost:3333",
+    "http://127.0.0.1:3333",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
